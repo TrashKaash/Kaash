a82068b217fa3230e491d2be21198299f2633bbe
TrashKaash <144768890+TrashKaash@users.noreply.github.com>
2026-08-27 06:46:50 -0600

    Create disabledCommands.js

---
+++ src/config/disabledCommands.js
@@ -0,0 +1,46 @@
+// Commands that are temporarily disabled.
+// Their files remain in the project and can be re-enabled later.
+
+export const disabledCommands = new Set([
+    // Economy
+    'balance',
+    'beg',
+    'buy',
+    'crime',
+    'daily',
+    'deposit',
+    'economy',
+    'eleaderboard',
+    'fish',
+    'gamble',
+    'inventory',
+    'mine',
+    'pay',
+    'rob',
+    'shop',
+    'withdraw',
+    'work',
+
+    // Birthday
+    'birthday',
+
+    // Giveaways
+    'gcreate',
+    'gdelete',
+    'gend',
+    'greroll',
+
+    // Leveling
+    'leaderboard',
+    'level',
+
+    // Utilities / tools
+    'baseconvert',
+    'calculate',
+    'hexcolor',
+    'shorten',
+    'unixtime',
+    'weather',
+    'todo',
+    'search',
+]);
